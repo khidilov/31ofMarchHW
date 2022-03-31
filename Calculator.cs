49366e6301d885d1c4035c1792c81a6b789c3241
khidilov <100746123+khidilov@users.noreply.github.com>
2022-03-31 23:49:08 +0400

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal static class Calculator
+    {
+        public static int Sum(int a, int b) { return a + b; }
+        public static int Substract(int a, int b) { return a - b; }
+
+        public static int Multiply (int a, int b) { return a * b; }
+        public static int Divide (int a, int b) { return a / b; }
+    }
+}
